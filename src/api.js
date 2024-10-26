16729f19e5f796736d058e6df014e9e454b1b670
Lud1024 <165453332+Lud1024@users.noreply.github.com>
2024-10-26 11:50:31 -0600

    api

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://nest-tickets.onrender.com/tickets',
-  baseURL: 'http://localhost:3000/tickets',
+  baseURL: 'https://nest-tickets.onrender.com/tickets',
+  //baseURL: 'http://localhost:3000/tickets',
 });
 
 export const createTicket = async (ticketData) => {
